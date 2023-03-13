33c560f1c7905e8ebe7f5f0cad7514b199f1fa3c
Shamansk1y <shamansky1225@gmail.com>
2023-03-13 21:37:42 +0100

    upd button category

--- static/js/gor_but.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('.dropdown-toggle').click(function() {
-    $(this).toggleClass('collapsed');
-    $(this).parent().toggleClass('show');
-    $(this).next('.dropdown-menu').toggleClass('show');
-    $(this).attr('aria-expanded', $(this).attr('aria-expanded') === 'false' ? 'true' : 'false');
-  });
-});
--- static/js/vert_but.js
+++ static/js/vert_but.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+  $('.dropdown-toggle').click(function(event) {
+    event.stopPropagation(); // отменяем всплытие клика по родительским элементам
+    $(this).toggleClass('collapsed');
+    $(this).parent().toggleClass('show');
+    $(this).next('.dropdown-menu').toggleClass('show');
+    $(this).attr('aria-expanded', $(this).attr('aria-expanded') === 'false' ? 'true' : 'false');
+  });
+});
+
+
 // получаем кнопку и навигационное меню по ID
 var button = document.getElementById("button-vertical");
 var menu = document.getElementById("navbar-vertical");
