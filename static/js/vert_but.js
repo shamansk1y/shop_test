ce72fc9df74a787a1d91e64665a9324020b0e04f
Shamansk1y <shamansky1225@gmail.com>
2023-03-13 21:55:14 +0100

    upd button category

--- static/js/vert_but.js
+++ static/js/vert_but.js
@@ -1,21 +1,42 @@
 $(document).ready(function() {
-  $('.dropdown-toggle').click(function(event) {
-    event.stopPropagation(); // отменяем всплытие клика по родительским элементам
+  $('.dropdown-toggle').click(function() {
     $(this).toggleClass('collapsed');
     $(this).parent().toggleClass('show');
     $(this).next('.dropdown-menu').toggleClass('show');
     $(this).attr('aria-expanded', $(this).attr('aria-expanded') === 'false' ? 'true' : 'false');
   });
-});
 
+  // получаем кнопку и навигационное меню по ID
+  var button = document.getElementById("button-vertical");
+  var menu = document.getElementById("navbar-vertical");
 
-// получаем кнопку и навигационное меню по ID
-var button = document.getElementById("button-vertical");
-var menu = document.getElementById("navbar-vertical");
+  // при клике на кнопку меняем классы и aria-expanded у кнопки и навигационного меню
+  button.addEventListener("click", function() {
+      button.classList.toggle("collapsed");
+      button.setAttribute("aria-expanded", button.classList.contains("collapsed") ? "false" : "true");
+      menu.classList.toggle("show");
+  });
 
-// при клике на кнопку меняем классы и aria-expanded у кнопки и навигационного меню
-button.addEventListener("click", function() {
-    button.classList.toggle("collapsed");
-    button.setAttribute("aria-expanded", button.classList.contains("collapsed") ? "false" : "true");
-    menu.classList.toggle("show");
+  // получаем все элементы списка подкатегорий
+  var subItems = document.querySelectorAll(".dropdown-submenu .dropdown-item");
+
+  // при наведении на элемент списка скрываем предыдущий открытый список
+  subItems.forEach(function(item) {
+    item.addEventListener("mouseleave", function() {
+      var previousMenu = this.closest(".dropdown-submenu").querySelector(".dropdown-menu.show");
+      if (previousMenu) {
+        previousMenu.classList.remove("show");
+      }
+    });
+  });
+
+  // при клике на элемент списка закрываем открытый список
+  subItems.forEach(function(item) {
+    item.addEventListener("click", function() {
+      var currentMenu = this.closest(".dropdown-menu");
+      if (currentMenu) {
+        currentMenu.classList.remove("show");
+      }
+    });
+  });
 });
