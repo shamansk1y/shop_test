b601b721abf70334cad4089964b478c6d858e8a4
Shamansk1y <shamansky1225@gmail.com>
2023-03-14 21:30:21 +0100

    upd blog_list

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from blogpost.models import Blog
+from main_page.context_data import get_common_context
+
 
 def blog_post_list(request):
 
     posts = Blog.objects.filter(is_visible=True).order_by('-pub_date')
-    return render(request, 'post_list.html', {'posts': posts})
+    data = {
+        'posts': posts
+    }
+    context_data = get_common_context()
+    data.update(context_data)
+    return render(request, 'post_list.html', context=data)
 
 def blog_post_detail(request, slug):
 
     post = get_object_or_404(Blog, slug=slug, is_visible=True)
-    return render(request, 'post_detail.html', {'post': post})
-
+    data = {
+        'post': post
+    }
+    context_data = get_common_context()
+    data.update(context_data)
+    return render(request, 'post_detail.html', context=data)
