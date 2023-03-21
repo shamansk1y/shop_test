cdc3a3ea5a39601d3a1a31f50b07aa14e0d8b68e
Shamansk1y <shamansky1225@gmail.com>
2023-03-21 01:02:14 +0100

    upd sort prod

--- shop/models.py
+++ shop/models.py
@@ -79,6 +79,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to=get_file_name, blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    position = models.PositiveIntegerField(default=1)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,20 @@
+from django.db.models import F
 from django.shortcuts import get_object_or_404, render
 from cart.cart import Cart
 from main_page.context_data import get_common_context
 from .models import Product
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 
 
 
 def product_list(request, category_slug=None):
     cart = Cart(request)
-    products = Product.objects.filter(available=True)
-    count = int(request.GET.get('count', 24))
+    products = Product.objects.filter(available=True).order_by('position')
+    count = int(request.GET.get('count', 24)) # 24 это количество товаров по умолчанию
     paginator = Paginator(products, count) # разбиваем на страницы по count товаров на странице
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+
     data = {
         'products': products,
         'cart': cart,
