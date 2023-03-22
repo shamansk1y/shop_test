561e4a1f94573db0fc31e369498ce24427975a4e
Shamansk1y <shamansky1225@gmail.com>
2023-03-22 23:24:00 +0100

    add slider in product images

--- static/js/photo_prod_car.js
+++ static/js/photo_prod_car.js
@@ -2,6 +2,11 @@ $(function() {
     // Получаем объект карусели
     var carousel = $('#product-carousel');
 
+    // Отключаем автоматическое переключение карусели
+    carousel.carousel({
+        pause: false
+    });
+
     // Обработчик для кнопки "назад"
     carousel.find('.carousel-control-prev').click(function() {
         carousel.carousel('prev');
