e3275c2b884e2cf15b907c84eeded3ce0517b6aa
Stas <113462863+shamansk1y@users.noreply.github.com>
2024-12-12 19:25:48 -0500

    Update settings.py

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -104,10 +104,6 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db = dj_database_url.config()
-DATABASES['default'].update(db)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
